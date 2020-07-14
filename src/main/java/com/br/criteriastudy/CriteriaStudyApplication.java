5fcb0737dbb2fa4accabd64e33edcc89204eb4b8
Vanderlei Leite Loureiro <desenvolvedor8@f5promotora.com.br>
2020-07-14 10:08:45 -0300

    first commit

---
+++ src/main/java/com/br/criteriastudy/CriteriaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.br.criteriastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriteriaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriteriaStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/criteriastudy/CriteriaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.criteriastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
