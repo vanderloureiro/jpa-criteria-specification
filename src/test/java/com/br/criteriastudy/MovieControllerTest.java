c3784296fa296ca80af24c7f13b8a2b7d8bda871
Vanderlei <desenvolvedor8@f5promotora.com.br>
2020-10-20 12:52:25 -0300

    Create test class

--- src/test/java/com/br/criteriastudy/MovieControllerTest.java
+++ src/test/java/com/br/criteriastudy/MovieControllerTest.java
@@ -1,13 +1,38 @@
 package com.br.criteriastudy;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.br.criteriastudy.entities.Movie;
+import com.br.criteriastudy.repositories.MovieRepository;
+import com.br.criteriastudy.services.DTO.MovieDTO;
 
-@SpringBootTest
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class MovieControllerTest {
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
-    public MovieControllerTest() {
+    @Autowired
+    private MovieRepository movieRepository; 
 
+    private MovieDTO movie;
+
+    @Before
+    public void start() {
+        this.movie = new MovieDTO();
     }
 
+    @After
+    public void end() {
+        this.movieRepository.deleteAll();
+    }
+    
 }
