52dccc863062b829c82e9ddfb31aa7134c6da77b
Vanderlei <desenvolvedor8@f5promotora.com.br>
2020-10-21 08:10:32 -0300

    Create get and post integration test

--- src/test/java/com/br/criteriastudy/MovieControllerTest.java
+++ src/test/java/com/br/criteriastudy/MovieControllerTest.java
@@ -1,16 +1,28 @@
 package com.br.criteriastudy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import com.br.criteriastudy.entities.CategoryEnum;
 import com.br.criteriastudy.entities.Movie;
 import com.br.criteriastudy.repositories.MovieRepository;
 import com.br.criteriastudy.services.DTO.MovieDTO;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -23,16 +35,43 @@ public class MovieControllerTest {
     @Autowired
     private MovieRepository movieRepository; 
 
-    private MovieDTO movie;
-
     @Before
     public void start() {
-        this.movie = new MovieDTO();
+        Movie movie = new Movie();
+        movie.setCategory(CategoryEnum.ACTION);
+        movie.setDirector("Tarantino");
+        movie.setSinopse("Django");
+        movie.setTitle("Django");
+        movie.setReleaseDate(LocalDate.of(2000, Month.JANUARY, 20));
+        this.movieRepository.save(movie);
     }
 
     @After
     public void end() {
         this.movieRepository.deleteAll();
+    }
+
+    @Test
+    public void getAllMovies() {
+        ResponseEntity<MovieDTO[]> response = this.testRestTemplate.exchange("/movie", HttpMethod.GET, null, MovieDTO[].class);
+        
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void createNewMovie() {
+        MovieDTO form = new MovieDTO();
+        form.setCategory(CategoryEnum.ACTION);
+        form.setDirector("Tarantino");
+        form.setSinopse("Kill Bill");
+        form.setTitle("Kill Bill");
+        form.setReleaseDate(LocalDate.of(2002, Month.JANUARY, 20));
+        HttpEntity<MovieDTO> httpEntity = new HttpEntity<>(form);
+
+        ResponseEntity<MovieDTO> response = this.testRestTemplate.exchange("/movie", HttpMethod.POST, httpEntity, MovieDTO.class);
+    
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody().getTitle(), "Kill Bill");
     }
     
 }
